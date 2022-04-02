eebe2a8c8dc0677a33116bee8356eb9024bc9a75
Matheus Alencar <matheus.garcia@bandtec.com.br>
2022-04-01 23:11:15 -0300

    added a new project and i did a register page

---
+++ ThothLibMobileApp/app/src/main/java/thothlib/mobile/thothlib_mobile_app/MainActivity.kt
@@ -0,0 +1,18 @@
+package thothlib.mobile.thothlib_mobile_app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun browsePage(V: View) {
+        val registerScreen = Intent(this, Register::class.java)
+        startActivity(registerScreen)
+    }
+}
---
+++ ThothLibMobileApp/app/src/main/java/thothlib/mobile/thothlib_mobile_app/SimpleHeader.kt
@@ -0,0 +1,13 @@
+package thothlib.mobile.thothlib_mobile_app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class SimpleHeader : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_simple_header)
+    }
+}
