d382257a66238674fb6cb884424b8f8d3a152599
Luis-Silva80 <70718471+Luis-Silva80@users.noreply.github.com>
2022-04-15 01:00:21 -0300

    feature: add tela de info de livros

---
+++ ThothLibMobileApp/app/src/main/java/thothlib/mobile/thothlib_mobile_app/InfoLivro.kt
@@ -0,0 +1,11 @@
+package thothlib.mobile.thothlib_mobile_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InfoLivro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info_livro)
+    }
+}
