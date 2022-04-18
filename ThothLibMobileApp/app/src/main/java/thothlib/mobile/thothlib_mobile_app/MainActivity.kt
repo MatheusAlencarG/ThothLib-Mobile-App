226dbe66b259a7c8a882582e3395cae1dd23443f
Matheus Alencar <matheus.garcia@bandtec.com.br>
2022-04-17 22:33:52 -0300

    integration login/register

--- ThothLibMobileApp/app/src/main/java/thothlib/mobile/thothlib_mobile_app/MainActivity.kt
+++ ThothLibMobileApp/app/src/main/java/thothlib/mobile/thothlib_mobile_app/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun browsePage(V: View) {
-        val registerScreen = Intent(this, InfoLivro::class.java)
+        val registerScreen = Intent(this, Login::class.java)
         startActivity(registerScreen)
     }
 
