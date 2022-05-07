55ab9249d8c5f15a05611289435798d6dcf11680
lucasPereira8243 <lucas.pereira@maeztra.com>
2022-05-07 14:35:42 -0300

    adding fragments

---
+++ ThothLibMobileApp/app/src/main/java/thothlib/mobile/thothlib_mobile_app/NewUser.kt
@@ -0,0 +1,9 @@
+package thothlib.mobile.thothlib_mobile_app
+
+data class NewUser(
+    val nome: String,
+    val cpf: String,
+    val email: String,
+    val telefone: String,
+    val senha: String
+)
--- ThothLibMobileApp/app/src/main/java/thothlib/mobile/thothlib_mobile_app/User.kt
+++ ThothLibMobileApp/app/src/main/java/thothlib/mobile/thothlib_mobile_app/User.kt
@@ -1,9 +1,14 @@
 package thothlib.mobile.thothlib_mobile_app
 
-data class User(
+data class User (
     val nome: String,
     val cpf: String,
     val email: String,
     val telefone: String,
-    val senha: String
+    val senha: String,
+    val usuarioAdmin: Int,
+    val pontos: Int,
+    val qtdLivrosLidos: Int,
+    val qtdResenhas: Int,
+    val livrosReservados: ReservedBooks,
 )
